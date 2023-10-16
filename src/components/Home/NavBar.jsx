8d9b65a83daf0765be2e6e62c7eebc34bd145cfa
Nurpolat Yerkinbayev <nurbilim.kz@gmail.com>
2023-10-16 20:41:51 +0600

    Separates the navigation links div in NavBar into a new component called NavLinks.
    Other minor changes.

--- src/components/Home/NavBar.jsx
+++ src/components/Home/NavBar.jsx
@@ -1,62 +1,23 @@
 import React, { useContext } from "react";
-import DropdownMenu from './navBarComponents/DropdownMenu'
-import { Link } from "react-router-dom";
-import ProfileDropDown from "./navBarComponents/ProfileDropDown";
-import pageUrls from "../../utils/pageUrls";
 import { AuthContext } from "../appContext/authContext";
+import NavLinks from "./navBarComponents/NavLinks";
 
 const NavBar = ({ titleText, logoImg }) => {
 
   const { isLoggedIn, setLoggedIn } = useContext(AuthContext)
-  console.log('NavBar isLoggedIn: ' + isLoggedIn);
-  console.log('NavBar setLoggedIn: ' + setLoggedIn);
-
-  const navLinkClassName = 'text-white text-sm hover:underline focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-red-700 focus:ring-white'
 
   return (
     <div className="bg-gray-800 grow-0">
       <div className="max-w-screen-xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="flex items-center justify-between h-14">
-        <div className="flex items-center">
-            {logoImg !== null && (
-                <div className="mr-4">{logoImg}</div>
-            )}
-            <span className="text-2xl text-white font-bold">{titleText}</span>
-        </div>
+        <div className="flex items-center justify-between py-2">
+          <div className="flex items-center">
+              {logoImg !== null && (
+                  <div className="mr-4">{logoImg}</div>
+              )}
+              <span className="text-2xl text-white font-bold">{titleText}</span>
+          </div>
 
-        <div className="space-x-6">
-
-          <Link to={pageUrls.home} className={navLinkClassName}>
-              Home
-          </Link>
-
-          {!isLoggedIn &&
-            <>
-              <Link to={pageUrls.login} className={navLinkClassName}>
-                  Sign in
-              </Link>
-              <Link to={pageUrls.register} className={navLinkClassName}>
-                  Sign up
-              </Link> 
-            </>
-          }
-
-          <Link to={pageUrls.services} className={navLinkClassName}>
-              Services
-          </Link>
-          <Link to={pageUrls.contacts} className={navLinkClassName}>
-              Contacts
-          </Link>
-          <Link to={pageUrls.about} className={navLinkClassName}>
-              About me
-          </Link>
-          
-          <DropdownMenu />
-
-          { isLoggedIn &&
-            <ProfileDropDown />
-          }
-        </div>
+          <NavLinks />
         </div>
       </div>
     </div>
---
+++ src/components/Home/navBarComponents/NavLinks.jsx
@@ -0,0 +1,50 @@
+import React, {useContext} from 'react'
+import { AuthContext } from '../../appContext/authContext'
+import { Link } from 'react-router-dom'
+import pageUrls from '../../../utils/pageUrls'
+import DropdownMenu from './DropdownMenu'
+
+const NavLinks = () => {
+
+  const { isLoggedIn, setLoggedIn } = useContext(AuthContext)
+
+  const navLinkClassName = 'text-white text-sm hover:underline focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-red-700 focus:ring-white'
+
+  return (
+    <div className="flex gap-4 w-1/3 flex-wrap justify-center">
+
+      <Link to={pageUrls.home} className={navLinkClassName}>
+          Home
+      </Link>
+
+      {!isLoggedIn &&
+        <>
+          <Link to={pageUrls.login} className={navLinkClassName}>
+              Sign in
+          </Link>
+          <Link to={pageUrls.register} className={navLinkClassName}>
+              Sign up
+          </Link> 
+        </>
+      }
+
+      <Link to={pageUrls.services} className={navLinkClassName}>
+          Services
+      </Link>
+      <Link to={pageUrls.contacts} className={navLinkClassName}>
+          Contacts
+      </Link>
+      <Link to={pageUrls.about} className={navLinkClassName}>
+          About me
+      </Link>
+      
+      <DropdownMenu />
+
+      { isLoggedIn &&
+        <ProfileDropDown />
+      }
+    </div>
+  )
+}
+
+export default NavLinks
